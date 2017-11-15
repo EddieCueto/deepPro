e7f91c67e1f59f9935530f4e625572889b49d1da
Eddie <cueto303@gmail.com>
2017-11-15 17:04:11 -0600

    small changes to code

--- deepPro.hs
+++ deepPro.hs
@@ -1,19 +1,18 @@
-import Codec.Compression.GZip (decompress)
 import qualified Data.ByteString.Lazy as BS
-
+import Codec.Compression.GZip (decompress)
+import System.Random
 import Control.Monad
 import Data.Functor
-import Data.Ord
 import Data.List
-import System.Random
+import Data.Ord
 
 gauss scale = do
   x1 <- randomIO
   x2 <- randomIO
   return $ scale * sqrt (-2 * log x1) * cos (2 * pi * x2)
 
-newBrain :: [Int] -> IO [([Float], [[Float]])]
-newBrain szs@(_:ts) = zip (flip replicate 1 <$> ts) <$>
+neuron :: [Int] -> IO [([Float], [[Float]])]
+neuron szs@(_:ts) = zip (flip replicate 1 <$> ts) <$>
   zipWithM (\m n -> replicateM n $ replicateM m $ gauss 0.01) szs ts
 
 relu = max 0
@@ -27,12 +26,12 @@ feed = foldl' (((relu <$>) . ) . zLayer)
 revaz xs = foldl' (\(avs@(av:_), zs) (bs, wms) -> let
   zs' = zLayer av (bs, wms) in ((relu <$> zs'):avs, zs':zs)) ([xs], [])
 
-dCost a y | y == 1 && a >= y = 0
+dervCost a y | y == 1 && a >= y = 0
           | otherwise        = a - y
 
 deltas xv yv layers = let
   (avs@(av:_), zv:zvs) = revaz xv layers
-  delta0 = zipWith (*) (zipWith dCost av yv) (relu' <$> zv)
+  delta0 = zipWith (*) (zipWith dervCost av yv) (relu' <$> zv)
   in (reverse avs, f (transpose . snd <$> reverse layers) zvs [delta0]) where
     f _ [] dvs = dvs
     f (wm:wms) (zv:zvs) dvs@(dv:_) = f wms zvs $ (:dvs) $
@@ -40,11 +39,11 @@ deltas xv yv layers = let
 
 eta = 0.002
 
-descend av dv = zipWith (-) av ((eta *) <$> dv)
+stGD av dv = zipWith (-) av ((eta *) <$> dv)
 
 learn xv yv layers = let (avs, dvs) = deltas xv yv layers
-  in zip (zipWith descend (fst <$> layers) dvs) $
-    zipWith3 (\wvs av dv -> zipWith (\wv d -> descend wv ((d*) <$> av)) wvs dv)
+  in zip (zipWith stGD (fst <$> layers) dvs) $
+    zipWith3 (\wvs av dv -> zipWith (\wv d -> stGD wv ((d*) <$> av)) wvs dv)
       (snd <$> layers) avs dvs
 
 getImage s n = fromIntegral . BS.index s . (n*28^2 + 16 +) <$> [0..28^2 - 1]
@@ -61,7 +60,7 @@ main = do
     ,  "t10k-images-idx3-ubyte.gz"
     ,  "t10k-labels-idx1-ubyte.gz"
     ]
-  b <- newBrain [784, 30, 10]
+  b <- neuron [784, 30, 10]
   n <- (`mod` 10000) <$> randomIO
   putStr . unlines $
     take 28 $ take 28 <$> iterate (drop 28) (render <$> getImage testI n)
@@ -79,7 +78,7 @@ main = do
 
   forM_ bs $ putStrLn . unlines . zipWith cute [0..9] . feed example
 
-  putStrLn $ "best guess: " ++ show (bestOf $ feed example smart)
+  putStrLn $ "Network guess: " ++ show (bestOf $ feed example smart)
 
   let guesses = bestOf . (\n -> feed (getX testI n) smart) <$> [0..9999]
   let answers = getLabel testL <$> [0..9999]
